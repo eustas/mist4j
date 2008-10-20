f13d01f1b1682754ac37542754a766b2a5cf87c0
eustas.ru <eustas.ru@40793848-9e84-11dd-8b36-1b7024100a8e>
2008-10-20 13:13:00 +0000

    Added simple tests for root library class

---
+++ src/test/java/ru/eustas/mist4j/TemplateLoaderTest.java
@@ -0,0 +1,182 @@
+package ru.eustas.mist4j;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+
+import junit.framework.Assert;
+
+public class TemplateLoaderTest {
+
+	public static class FakeResource implements ITemplateResource {
+		private final String text;
+
+		public FakeResource(String _text) {
+			text = _text;
+		}
+
+		@Override
+		public String getContent() throws IOException {
+			return text;
+		}
+
+		// @Override
+		// public boolean isActual() {
+		// return true;
+		// }
+	}
+
+	public static class FakeSource implements ITemplateSource {
+		private String text;
+
+		public FakeSource(String _text) {
+			text = _text;
+		}
+
+		@Override
+		public ITemplateResource getResource(String fileName) {
+			return new FakeResource(text);
+		}
+	}
+
+	public static class FakeRenderer implements IFastRenderer {
+		private StringWriter writer;
+
+		public FakeRenderer() {
+			writer = new StringWriter();
+		}
+
+		public void renderStub() {
+			writer.write("Cruel");
+		}
+
+		@Override
+		public Writer getOut() {
+			return writer;
+		}
+	}
+
+	public void testCommonTemplate() throws Exception {
+		FakeSource fakeSource = new FakeSource("Hello {stub} World");
+		TemplateLoader loader = new TemplateLoader(fakeSource);
+		ITemplate template = loader.loadTemplate("fake", FakeRenderer.class);
+		FakeRenderer victim = new FakeRenderer();
+		template.process(victim);
+		Assert.assertEquals("Hello Cruel World", victim.getOut().toString());
+	}
+
+	public void testTaillessTemplate() throws Exception {
+		FakeSource fakeSource = new FakeSource("Hello {stub}");
+		TemplateLoader loader = new TemplateLoader(fakeSource);
+		ITemplate template = loader.loadTemplate("fake", FakeRenderer.class);
+		FakeRenderer victim = new FakeRenderer();
+		template.process(victim);
+		Assert.assertEquals("Hello Cruel", victim.getOut().toString());
+	}
+
+	public void testHeadlessTemplate() throws Exception {
+		FakeSource fakeSource = new FakeSource("{stub} World");
+		TemplateLoader loader = new TemplateLoader(fakeSource);
+		ITemplate template = loader.loadTemplate("fake", FakeRenderer.class);
+		FakeRenderer victim = new FakeRenderer();
+		template.process(victim);
+		Assert.assertEquals("Cruel World", victim.getOut().toString());
+	}
+
+	public void testPlainTemplate() throws Exception {
+		FakeSource fakeSource = new FakeSource("Hello World");
+		TemplateLoader loader = new TemplateLoader(fakeSource);
+		ITemplate template = loader.loadTemplate("fake", FakeRenderer.class);
+		FakeRenderer victim = new FakeRenderer();
+		template.process(victim);
+		Assert.assertEquals("Hello World", victim.getOut().toString());
+	}
+
+	public void testFormatTemplate() throws Exception {
+		FakeSource fakeSource = new FakeSource("{stub}{stub}");
+		TemplateLoader loader = new TemplateLoader(fakeSource);
+		ITemplate template = loader.loadTemplate("fake", FakeRenderer.class);
+		FakeRenderer victim = new FakeRenderer();
+		template.process(victim);
+		Assert.assertEquals("CruelCruel", victim.getOut().toString());
+	}
+
+	public void testIncorrectTemplate() throws Exception {
+		FakeSource fakeSource = new FakeSource("{stub}{buts}");
+		TemplateLoader loader = new TemplateLoader(fakeSource);
+		ITemplate template = loader.loadTemplate("fake", FakeRenderer.class);
+		FakeRenderer victim = new FakeRenderer();
+		NoSuchMethodError ex = null;
+		try {
+			template.process(victim);
+		} catch (NoSuchMethodError e) {
+			ex = e;
+		}
+		Assert.assertNotNull(ex);
+	}
+
+	public void testReloadTemplate() throws Exception {
+		FakeSource fakeSource = new FakeSource("Text1");
+		TemplateLoader loader = new TemplateLoader(fakeSource);
+		ITemplate template1 = loader.loadTemplate("fake", FakeRenderer.class);
+		fakeSource.text = "Text2";
+		ITemplate template2 = loader.loadTemplate("fake", FakeRenderer.class);
+		FakeRenderer victim1 = new FakeRenderer();
+		template1.process(victim1);
+		Assert.assertEquals("Text1", victim1.getOut().toString());
+		FakeRenderer victim2 = new FakeRenderer();
+		template2.process(victim2);
+		Assert.assertEquals("Text2", victim2.getOut().toString());
+	}
+
+	// public void testPerformance1() throws Exception {
+	// FakeSource fakeSource = new FakeSource(
+	// "text text text text text text text text text text text text {stub}"
+	// + "text text text text text text text text text text text text {stub}"
+	// + "text text text text text text text text text text text text {stub}"
+	// + "text text text text text text text text text text text text {stub}");
+	// TemplateLoader loader = new TemplateLoader(fakeSource);
+	// ITemplate template = loader.loadTemplate("fake", FakeRenderer.class);
+	// for (int i = 0; i < 100000; i++) {
+	// FakeRenderer victim = new FakeRenderer();
+	// template.process(victim);
+	// victim.writer.toString();
+	// }
+	// System.gc();
+	// Thread.sleep(500);
+	// long n0 = System.nanoTime();
+	// for (int i = 0; i < 100000; i++) {
+	// FakeRenderer victim = new FakeRenderer();
+	// template.process(victim);
+	// victim.writer.toString();
+	// }
+	// long n1 = System.nanoTime();
+	// System.out.println("~= " + (((n1 - n0) / 100000) / 1000.0) + "mks");
+	// }
+	//
+	// public void testPerformance2() throws Exception {
+	// FakeSource fakeSource = new FakeSource(
+	// "text text text text text text text text text text text text "
+	// + "text text text text text text text text text text text text "
+	// + "text text text text text text text text text text text text {stub}"
+	// + "text text text text text text text text text text text text ");
+	// TemplateLoader loader = new TemplateLoader(fakeSource);
+	// ITemplate template = loader.loadTemplate("fake", FakeRenderer.class);
+	// for (int i = 0; i < 100000; i++) {
+	// FakeRenderer victim = new FakeRenderer();
+	// template.process(victim);
+	// victim.writer.toString();
+	// }
+	// System.gc();
+	// Thread.sleep(500);
+	// long n0 = System.nanoTime();
+	// for (int i = 0; i < 100000; i++) {
+	// FakeRenderer victim = new FakeRenderer();
+	// template.process(victim);
+	// victim.writer.toString();
+	// }
+	// long n1 = System.nanoTime();
+	// System.out.println("~= " + (((n1 - n0) / 100000) / 1000.0) + "mks");
+	// }
+
+}
